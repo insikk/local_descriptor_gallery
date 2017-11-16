4c4f53ee622f235d8354067c23002226c8a27b2f
insikk <insik92@gmail.com>
2017-11-16 22:09:20 +0900

    get ransac as image byte

--- utils/matcher.py
+++ utils/matcher.py
@@ -2,7 +2,7 @@
 
 import cv2
 import numpy as np
-from skimage.measure import ransac
+from skimage.measure import ransac as _ransac
 from skimage.transform import AffineTransform
 import matplotlib.pyplot as plt
 
@@ -50,8 +50,7 @@ def get_matches(des1, des2, method='BRUTE_FORCE', is_binary_descriptor=True):
     return matches
 
 
-
-def draw_ransac(img1, img2, detector, descriptor_extractor, is_binary_descriptor, match_method, description):
+def ransac(img1, img2, detector, descriptor_extractor, is_binary_descriptor, match_method, description):
     # find the keypoints and descriptors
     kp1 = detector.detect(img1,None)
     kp1, des1 = descriptor_extractor.compute(img1, kp1)
@@ -82,7 +81,7 @@ def draw_ransac(img1, img2, detector, descriptor_extractor, is_binary_descriptor
     locations_2_to_use = np.array(locations_2_to_use)
 
     # Perform geometric verification using RANSAC.
-    model_robust, inliers = ransac(
+    model_robust, inliers = _ransac(
       (locations_1_to_use, locations_2_to_use),
       AffineTransform,
       min_samples=5,
@@ -96,10 +95,30 @@ def draw_ransac(img1, img2, detector, descriptor_extractor, is_binary_descriptor
         inlier_match.append(matches[idx])
 
     # Does ransac consider size and orientation of keypoints?
-    ransac_img = cv2.drawMatches(img1, kp1, img2, kp2, inlier_match, None, flags=0)
-    ransac_img = cv2.cvtColor(ransac_img, cv2.COLOR_BGR2RGB);
+    ransac_img = cv2.drawMatches(img1, kp1, img2, kp2, inlier_match, None, flags=0)    
 
+    if inliers is None:
+        score = 0
+    else:
+        score = len(inliers)
+
+    return ransac_img, score
+
+
+def draw_ransac(img1, img2, detector, descriptor_extractor, is_binary_descriptor, match_method, description):
+    ransac_img, score = ransac(img1, img2, detector, descriptor_extractor, is_binary_descriptor, match_method, description)
+    ransac_img = cv2.cvtColor(ransac_img, cv2.COLOR_BGR2RGB)
     plt.figure(figsize=(16, 12))
     plt.title(description)
     plt.imshow(ransac_img)
     plt.show()
+    return score
+
+
+def get_ransac_image_byte(img1, img2, detector, descriptor_extractor, is_binary_descriptor, match_method, description):
+    """
+    from image, return sansac image and number of inliner as score.
+    """
+    ransac_img, score = ransac(img1, img2, detector, descriptor_extractor, is_binary_descriptor, match_method, description)
+    image_byte = cv2.imencode('.png', ransac_img)[1].tostring()
+    return image_byte, score
